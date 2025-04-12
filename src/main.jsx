b41b01732cac30c6487052a417c203b2b263c7bf
piyush kumar <147308217+piyushkumar980@users.noreply.github.com>
2025-04-12 20:14:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import App from './App.jsx'
 import './i18n'
-import './index.css'
 
 const root = createRoot(document.getElementById('root'))
 
